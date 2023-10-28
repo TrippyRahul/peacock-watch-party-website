479f974f73fa4c4b290d9e9ff92a4066fbca4060
TrippyGaurav <gaurav.trippybug@gmail.com>
2023-10-28 17:52:36 +0530

    add target blank

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -3,7 +3,10 @@ import styles from "../styles/button.module.scss";
 
 const Button = () => {
   return (
-    <a href="https://chrome.google.com/webstore/detail/watchottparty/akbbdmilignoplkhminiaecccigmpdmo?hl=en-US">
+    <a
+      href="https://chrome.google.com/webstore/detail/watchottparty/akbbdmilignoplkhminiaecccigmpdmo?hl=en-US"
+      target="_blank"
+    >
       <button className={styles.btn}>
         Add to Chrome
         <svg
